28c40359ec4640a722ed7dd2329d7dada739b8e7
jdboyd3 <60266624+jdboyd3@users.noreply.github.com>
2020-02-05 07:15:57 -0900

    Update dotcross.cpp

--- hw1/dotcross.cpp
+++ hw1/dotcross.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+int G=0; 
+int i;
+cout<<"Enter a sequence of positive integers, ending with zero. I will print the greatest positive number entered."<<endl;
+cin >> i;
+
+if(i>=1){
+while(i>=1){
+cin >> i;
+  if(i>G)
+  {
+  G=i;
+  }
+}
+cout <<"Your greatest input was: " <<G<< endl;
+}
+
+else{
+    cout<<"No positive integers were entered"<<endl;
+}
+return 0;
+}
 
