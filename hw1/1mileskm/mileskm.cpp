3ef725734fa45fc5e02eeafd6d25d99f9ac3a623
jdboyd3 <60266624+jdboyd3@users.noreply.github.com>
2020-02-05 22:30:02 -0900

    Update mileskm.cpp

--- hw1/1mileskm/mileskm.cpp
+++ hw1/1mileskm/mileskm.cpp
@@ -1,3 +1,9 @@
+// Jakobe Boyd
+// CS 201X
+//2/5/20
+//This function accepts a value and converts it from miles to kilometers
+
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -5,8 +11,8 @@ using namespace std;
 int main()
 {
 cout<<"Input a number to be converted from miles to kilometers."<<endl;
-int mile_input;
-float kilometer=1.609;
+int mile_input; //user input
+float kilometer=1.609; //value used to multiply user input by and get conversion
 cin>>mile_input;
 cout<<mile_input <<" mile(s) is equivalent to "<<kilometer*mile_input<<" kilometer(s)."<<endl;
 return 0;
