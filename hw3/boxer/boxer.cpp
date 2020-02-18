cd4ccb37da6e32f4ba7aff7cf768c0ed0e814a14
jdboyd3 <60266624+jdboyd3@users.noreply.github.com>
2020-02-17 22:47:50 -0900

    Update boxer.cpp

--- hw3/boxer/boxer.cpp
+++ hw3/boxer/boxer.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  int size;
+  string s;
+  int i;
+  cout << "Please input a string and a size integer"<< endl;
+
+  cout << "Size?: "<< endl;
+  cin >> size;
+if(size>1){
+  cout << "String?: "<< endl;
+  cin >> s;
+  for(int i=0;s.length()>i;i++){
+      
+    for(int a = 0; a < size; a++){
+        }
+    }
+}
+    else{
+        cout<<"Please enter a size larger than 0"<< endl;
+    }
+}
 
