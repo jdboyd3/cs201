f67778ad4f93f32fdc3d15659c7913800e5d5163
jdboyd3 <60266624+jdboyd3@users.noreply.github.com>
2020-02-19 19:15:08 -0900

    Update boxer.hpp

--- hw3/boxer/boxer.hpp
+++ hw3/boxer/boxer.hpp
@@ -1,1 +1,17 @@
+/* boxer.hpp
+ * 
+ * Jakobe Boyd
+ *
+ * 2/19/2020
+ */
 
+
+#ifndef BOXER_HPP
+#define BOXER_HPP
+#include <iostream>
+#include <string>
+using namespace std;
+
+int x=1
+
+#endif /* BOXER_HPP */
