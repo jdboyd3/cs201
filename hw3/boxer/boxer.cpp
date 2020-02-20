de10a556cea11850b18f88c0b1c966a36b345c6e
jdboyd3 <60266624+jdboyd3@users.noreply.github.com>
2020-02-19 20:44:07 -0900

    Update boxer.cpp

--- hw3/boxer/boxer.cpp
+++ hw3/boxer/boxer.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main()
 {
 int x=1;
+//do while so that the program continues running
 do{
 string str;
 cout<<"Enter a string: ";
@@ -21,7 +22,7 @@ for(int j=0;j<2*size+str.length();j++)
 cout<<"*";
 cout<<endl;
 
-}
+}// asterisks
 for(int i=0;i<size;i++)
 cout<<"*";
 cout<<str;
@@ -34,11 +35,11 @@ for(int j=0;j<2*size+str.length();j++)
 cout<<"*";
 cout<<endl;
 
-}
-cout<<"Press 1 and enter to start making another box: ";
+} // once the user has input their initial string and box size, the code below keeps the program running for more inputs
+cout<<"Press 1 and enter to start making another box, press any other key and enter to quit: ";
 cin>>x;
 }
-while(ch==1);
+while(x==1);
 return 0;
 
 }
