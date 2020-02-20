3ef8f6636bcdf0edcb711c563a12962521798319
jdboyd3 <60266624+jdboyd3@users.noreply.github.com>
2020-02-19 19:47:56 -0900

    Create kelvin.cpp

---
+++ hw3/kelvin.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+double ctok(double c)
+{
+    double k = c + 273.15; // k should be float instead of int to prevent truncation, changed 'int k = c + 273.15' to 'double k = c +273.15'
+    return k; // return should be reutrning a value, so changed 'return int' to 'return k'
+}
+
+int main()
+{
+    double c = 0;
+    cin>>c; //d is a misinput, it should be c, changed 'cin>>d' to 'cin>>c'
+    if(c>=-273.15)
+    {
+        
+    double k =ctok(c);  // want to call the value c, not string "c", changed 'double k =ctok("c")'' to 'double k =ctok(c)'
+    cout<<k<< '/n' ; //cout shouldnt be capitalized
+    }
+    else
+    {
+        cout<<"Please enter a value greater than or equal to -273.15"<<endl;
+    }
+return 0;
+}
