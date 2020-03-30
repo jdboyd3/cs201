2d426d21ab12ebbac4ac16fd3f9a1241c6720a6f
jdboyd3 <60266624+jdboyd3@users.noreply.github.com>
2020-03-29 17:34:25 -0800

    Create floating_point.hpp

---
+++ lab 19/floating_point.hpp
@@ -0,0 +1,32 @@
+/**
+ * @file     floating_point.hpp
+ * @author   Jakobe Boyd
+ * @version  Mar 1, 2019
+ * John Quan 
+ *
+ * Useful trigonometric functions.
+ */
+
+#ifndef FLOATING_POINT_HPP_
+#define FLOATING_POINT_HPP_
+using namespace std;
+#include <vector>
+#include<iomanip>
+
+/**
+ * Return a vector of radians where the angle in degrees equates
+ * to the index, and the element at that index holds the equivalent
+ * angle in radians. Therefore,  DEGREE == INDEX
+ * For example:
+ *    vector<double> radians = degreesToRadians(2);
+ * Contains:  INDEX     RADIAN
+ *            0         0
+ *            1         0.0174533
+ *            2         0.0349066
+ *
+ * @param degrees from 0 to this value
+ * @return Vector of radians
+ */
+std::vector<double> degreesToRadians(int degree);
+
+#endif /* FLOATING_POINT_HPP_ */
